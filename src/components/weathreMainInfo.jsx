149e51c47a269b0203576e9d6b5cd2c78563471d
eddyjav <eddy.j.av@gmail.com>
2023-01-07 15:04:57 -0500

    :sunny: weatherMainInfo modified google maps iframe

--- src/components/weathreMainInfo.jsx
+++ src/components/weathreMainInfo.jsx
@@ -17,6 +17,7 @@ export default function WheaterMainInfo({ weather }) {
           <div>{weather?.current.temp_c}°</div>
         </div>
       </div>
+
       <iframe
         src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d317715.7119527189!2d${weather?.location.lon}!3d${weather?.location.lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x!2s${weather?.location.name}!5e0!3m2!1ses-419!2sec!4v1672962393864!5m2!1ses-419!2sec`}
         width="600"
